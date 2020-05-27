782d3cfc68b3914cb6aeee3ef915f88a8a641916
Maxwell Martins Lima Monteiro <maxwba@MBP-de-Maxwell.lan>
2020-05-27 09:53:20 +0100

    Tasklist card created

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Box } from '@chakra-ui/core';
+import { Box, Text } from '@chakra-ui/core';
 import { useSelector } from 'react-redux';
 
 import TasklistNav from './tasklistNav';
@@ -12,9 +12,17 @@ const Tasklist = () => {
     <Box marginLeft="20px" d="flex" flexDirection="column" height="100vh" position="relative">
       <TasklistNav />
       <Box overflowY="auto" position="absolute" top="90px" left="0px" right="0px" bottom="0px">
-        <ul>
-          {state.posts.map((post) => <li key={post.id}>{post.title}</li>)}
-        </ul>
+        <Box>
+          {state.posts.map((post) => (
+            <Box>
+              <Box key={post.id} border="solid 1px gray" h="30px" d="flex" justifyContent="space-between" padding="5px" alignItems="center">
+                <Text fontWeight="700" fontSize="20px">{post.title}</Text>
+                <Text fontWeight="300" fontSize="15px">{post.date}</Text>
+              </Box>
+              <Box border="solid 1px gray" borderBottomRightRadius="5px" borderBottomLeftRadius="5px" padding="5px" marginBottom="15px"><Text>{post.message}</Text></Box>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
